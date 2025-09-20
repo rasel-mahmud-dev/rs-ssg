d1accfa68305759dd2efd683a21e53f91bc0b694
rasel.mahmmud <rasel.mahmud.dev@gmail.com>
2025-09-20 21:40:02 +0600

    refactor: rename files and update imports; add new plugins for enhanced functionality

--- lib/head.jsx
+++ lib/head.jsx
@@ -1,10 +1,8 @@
 import React, { useEffect } from 'react';
 
 export const Head = (props) => {
-    console.log(props, "jjjjjjjjjjj");
     return null;
 };
 
-// Optional: Add display name for better debugging
 Head.displayName = 'Head';
 
--- playground/src/routes.js
+++ playground/src/routes.js
@@ -1,24 +1,37 @@
+// import {lazy} from "react";
 import {lazy} from "react";
 
 const routes = [
     {
-        path: '/:id',
-        component: lazy(() => import('./ItemPage.jsx')),
-        entry: "/src/ItemPage.jsx",
-        title: 'ItemPage Screen'
-    },
-    {
         path: '/',
-        component: lazy(() => import('./Home.jsx')),
-        entry: "/src/Home.jsx",
+        component: lazy(() => import('./pages/Home.jsx')),
+        entry: "/src/pages/Home.jsx",
         title: 'Home Screen'
     },
+    // {
+    //     path: '/:id',
+    //     component: lazy(() => import('./pages/ItemPage.jsx')),
+    //     entry: "/src/pages/ItemPage.jsx",
+    //     title: 'ItemPage Screen'
+    // },
+    // {
+    //     path: '/examples',
+    //     entry: "/src/pages/Examples.jsx",
+    //     component: lazy(() => import('./pages/Examples.jsx')),
+    //     title: 'Examples'
+    // },
     {
-        path: '/about',
-        component: lazy(() => import('./Home.jsx')),
-        entry: "/src/About.jsx",
-        title: 'About Screen'
+        path: '/docs',
+        entry: "/src/pages/Docs.jsx",
+        component: lazy(() => import('./pages/Docs.jsx')),
+        title: 'Docs',
     }
+//     {
+//         path: '/about',
+//         component: lazy(() => import('./pages/About.jsx')),
+//         entry: "/src/pages/About.jsx",
+//         title: 'About Screen'
+//     }
 ]
 
 

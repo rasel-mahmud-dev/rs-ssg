ade81f794c4d1bd5b8f5a5b949cf7c4d9cec4849
rasel.mahmmud <rasel.mahmud.dev@gmail.com>
2025-11-12 15:52:07 +0600

    feat: Added support for head in global configuration to generate global meta tags at build time, and updated version to 1.3.0.

--- build.sh
+++ build.sh
@@ -1,4 +1,5 @@
-#tsc && node ./bin/rs-ssg.js  && cd playground && pnpm install && npm run build
+#  node ./bin/rs-ssg.js  && cd playground && pnpm install && npm run build
 
 
-rm -rf my-app && pnpm remove -g rs-ssg && pnpm link && rs-ssg init my-app && pnpm install
+
+pnpm remove -g rs-ssg && pnpm link && cd playground && pnpm install && npm run build
